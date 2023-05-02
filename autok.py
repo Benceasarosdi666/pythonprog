cf539a2fe2d8cad89b16bad018e617b85001c8d4
unknown <tanulo@t12g44.hunyadi.local>
2023-05-02 12:18:44 +0200

    dawqd

--- autok.py
+++ autok.py
@@ -1,9 +1,18 @@
 class Autok:
-    def def __init__(self, Ferohely, Nev, ar, Kategoria):
-      oszlop = sor.strip().split(";")  
-      self.Ferohely = int(oszlop[0])
-      self.Nev = oszlop[1] 
-      self.ar = int(oszlop[2]) 
-      self.Kategoria = oszlop[3] 
-fajl = open("ipl.txt","r",encoding="utf-8")
+  def __init__(self, sor):
+   oszlop = sor.split(";")
+   self.ferohely = int(oszlop[0])
+   self.nev = oszlop[1] 
+   self.ar = int(oszlop[2]) 
+   self.kategoria = oszlop[3]
 
+fajl = open("ipl.txt","r",encoding="utf-8")
+fajl.readline()
+k_lista = [Autok(sor.strip()) for sor in fajl]
+
+print(f"számuk: {len(k_lista)} db")
+
+osszeg = 0
+for k in k_lista:
+    osszeg = osszeg + k.ar
+print(f"Az autók száma: {osszeg} Ft")
